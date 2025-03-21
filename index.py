e1ee8f84c2744abb9f14bd069dcab1f2ed9cd24a
Mithilesh <68847934+mithilesh1627@users.noreply.github.com>
2025-03-21 15:42:20 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, jsonify
 import tensorflow_hub as hub
-
+from flask_cors import CORS
+import numpy
 app = Flask(__name__)
+CORS(app)
 
-# Load the pre-trained Universal Sentence Encoder model
+
 embed_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 @app.route('/embed', methods=['POST'])
 def get_embedding():
     try:
-        data = request.json  # Expecting JSON {"text": "your sentence here"}
+        data = request.json
         text = data.get("text")
 
         if not text:
             return jsonify({"error": "Text input is required"}), 400
 
-        # Generate embeddings
         embedding = embed_model([text])[0].numpy().tolist()
 
         return jsonify({"embedding": embedding})
