42da9837cb69c430f036247c03dcb2c0db6b005e
Mithilesh <68847934+mithilesh1627@users.noreply.github.com>
2025-03-28 12:50:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,4 +192,5 @@ def add_embedding():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
